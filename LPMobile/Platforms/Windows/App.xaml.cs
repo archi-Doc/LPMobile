9ed63db131ca78be464e103e49b5f9471cfe9894
archi-Doc <archidoc422@gmail.com>
2022-08-22 00:11:44 +0900

    ..

--- LPMobile/Platforms/Windows/App.xaml.cs
+++ LPMobile/Platforms/Windows/App.xaml.cs
@@ -1,7 +1,8 @@
 ﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
 
-using Microsoft.UI.Xaml;
-using Tinyhand;
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Windows.Graphics;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -16,30 +17,44 @@ public partial class App : MauiWinUIApplication
     public App()
     {
         // Prevents multiple instances.
-        var mutex = new System.Threading.Mutex(false, AppConst.MutexName);
+        /*var mutex = new System.Threading.Mutex(false, AppConst.MutexName);
         if (!mutex.WaitOne(0, false))
         {
             mutex.Close(); // Release mutex.
 
-            var prevProcess = Arc.WinAPI.Methods.GetPreviousProcess();
-            if (prevProcess != null)
-            {
-                var handle = prevProcess.MainWindowHandle; // The window handle that associated with the previous process.
-                if (handle == IntPtr.Zero)
-                {
-                    handle = Arc.WinAPI.Methods.GetWindowHandle(prevProcess.Id, string.Empty); // Get handle.
-                }
+            return; // Exit.
+        }*/
 
-                if (handle != IntPtr.Zero)
-                {
-                    Arc.WinAPI.Methods.ActivateWindow(handle);
-                }
+        var prevProcess = Arc.WinAPI.Methods.GetPreviousProcess();
+        if (prevProcess != null)
+        {
+            var handle = prevProcess.MainWindowHandle; // The window handle that associated with the previous process.
+            if (handle == IntPtr.Zero)
+            {
+                handle = Arc.WinAPI.Methods.GetWindowHandle(prevProcess.Id, string.Empty); // Get handle.
             }
 
-            return; // Exit.
+            if (handle != IntPtr.Zero)
+            {
+                Arc.WinAPI.Methods.ActivateWindow(handle);
+            }
         }
 
         this.InitializeComponent();
+
+        int WindowWidth = 600;
+        int WindowHeight = 500;
+        Microsoft.Maui.Handlers.WindowHandler.Mapper.AppendToMapping(nameof(IWindow), (handler, view) =>
+        {
+            var mauiWindow = handler.VirtualView;
+            var nativeWindow = handler.PlatformView;
+            nativeWindow.Activate();
+            IntPtr windowHandle = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
+            WindowId windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(windowHandle);
+            AppWindow appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+            appWindow.Resize(new SizeInt32(WindowWidth, WindowHeight));
+            appWindow.Title = "test";
+        });
     }
 
     protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
